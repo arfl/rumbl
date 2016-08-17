1316ce161b849da33037fa55c795dd029ebb3ad3
Andreas Riemer <andreas.riemer@gmail.com>
2016-08-17 20:27:59 +0200

    Adds user model

---
+++ web/models/user.ex
@@ -0,0 +1,22 @@
+defmodule Rumbl.User do
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ :empty) do
+    struct
+    |> cast(params, [:name, :username, :password_hash])
+    |> validate_required([:name, :username, :password_hash])
+    |> unique_constraint(:username)
+  end
+end
