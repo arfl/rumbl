f63f9c4463785650227ec5b81ce1c70b742b66a6
Andreas Riemer <andreas.riemer@gmail.com>
2016-08-18 17:21:28 +0200

    Adds heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rumbl, Rumbl.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "rumbl-arfl.herokuapp.com", port: 443, scheme: "https"],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
